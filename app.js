f6669bc1a77aad3dcee9a66cdf6cd69f5dc15f23
Guyliane <guyliane@live.fr>
2022-01-19 15:01:16 +0100

    fix responsive

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const btnMenu = document.querySelector('.btn-toggle-container');
 const allNavItem = document.querySelectorAll('.item-nav');
 const btn = document.querySelector('.btn1');
 
+const aproposContainer = document.querySelector('.card-about');
+
 
 const imgProfil = document.querySelector('.imgProfil');
 const unOs = document.querySelector('.imgOs');
@@ -14,7 +16,7 @@ gsap.from(imgProfil, { y: 200, duration: 1, ease: "elastic.out(1, 0.3)" })
 
 gsap.to(unOs, { 
     scrollTrigger : {
-        trigger : unOs,
+        trigger : aproposContainer,
         toggleActions : "restart pause resume none",
         scrub : 1
     },
