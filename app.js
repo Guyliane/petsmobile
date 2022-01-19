47ef93034fc4fb9b368ff5a3a44c36151884ecf5
Guyliane <guyliane@live.fr>
2022-01-19 13:07:00 +0100

    Premier commit du site Pets mobile

---
+++ app.js
@@ -0,0 +1,51 @@
+const menu = document.querySelector('.liste-nav');
+const btnMenu = document.querySelector('.btn-toggle-container');
+const allNavItem = document.querySelectorAll('.item-nav');
+const btn = document.querySelector('.btn1');
+
+
+const imgProfil = document.querySelector('.imgProfil');
+const unOs = document.querySelector('.imgOs');
+const ressort = document.querySelector('.ressort');
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(imgProfil, { y: 200, duration: 1, ease: "elastic.out(1, 0.3)" })
+
+gsap.to(unOs, { 
+    scrollTrigger : {
+        trigger : unOs,
+        toggleActions : "restart pause resume none",
+        scrub : 1
+    },
+    x: 1200, 
+    duration: 3, 
+    rotation : 80});
+
+gsap.to(ressort, { 
+    scrollTrigger : {
+        trigger : ressort,
+        toggleActions : "restart pause resume none",
+        scrub : 1
+    },
+    x: -1000,
+    y : -200, 
+    duration: 3, 
+    rotation : 80});
+
+/* #region  BOUTON HAMBURGER */
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active-menu')
+})
+
+allNavItem.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('active-menu')
+        btn.classList.toggle('active')
+    })
+})
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active')
+})
+/* #endregion */
